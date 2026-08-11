4b66a67dcf50c171555af8b46bc72226dac0c17f
abhishek714179 <abhishekmandal5284@gmail.com>
2026-08-11 13:13:53 +0530

    update

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello(){
-    return "Hello from CI/CD pipeline";
+    return "Hello from CI/CD pipeline...";
     }
 }
